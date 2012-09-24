58842d8ea92e9b5feeaa88cfb8e8067850730bd1
olyalevitskaya <tenshi-desu@mail.ru>
2012-09-24 14:21:20 +0600

    install git to VS2012

---
+++ os1/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include <fstream>
+    
+using namespace std;
+
+ifstream fin("code.txt");	
+ofstream flat("out.txt");
+
+int main()
+{
+
+	return 0;
+}
